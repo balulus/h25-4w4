9b8d3a2675f50367d8ba55063ce278ebadb43fa0
Valerie Therrien <e6238125@cmaisonneuve.qc.ca>
2025-03-12 18:03:49 -0400

    S8 - Finalisation du footer

--- footer.php
+++ footer.php
@@ -16,9 +16,15 @@
   	 				<li><a href="#">3800, Sherbrook est, Montréal, Québec, Canada, H1X 2A2</a></li>
   	 				<li><a href="#">Tel: (514)254-7131</a></li>
   	 			</ul>
+				   <?php get_search_form();   ?>
+
   	 		</div>
   	 		<div class="footer-col">
-               <?php get_search_form();   ?>
+  	 			<h4>Mission de l'exploration du manoir céleste</h4>
+  	 			<ul>
+  	 				<li>La Mission de l'Exploration du Manoir Céleste est une aventure mystérieuse et fascinante qui s'inscrit souvent dans des récits de fiction, où des aventuriers ou des explorateurs sont envoyés pour découvrir les secrets d'un manoir flottant ou caché dans les cieux.</li>
+  	 			</ul>
+
   	 		</div>
   	 	</div>
   	 </div>
