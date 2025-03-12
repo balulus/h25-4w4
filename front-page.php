93875a6cbb33498334379c0620cb188c5f75ddc9
Valerie Therrien <e6238125@cmaisonneuve.qc.ca>
2025-03-12 17:44:22 -0400

    S8 - Ajout du customizer hero_couleur

--- front-page.php
+++ front-page.php
@@ -2,20 +2,26 @@
 <?php 
 $hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
 $hero_background = get_theme_mod('hero_background', ''); 
+$hero_couleur = get_theme_mod('hero_couleur', '');
 ?>
+<style>
+    .hero__couleur{
+        color :  <?php echo $hero_couleur ?>;
+    } 
+</style>
     <section class="hero" style="background-image: url(<?php echo $hero_background ?>)">
         <div class="hero__contenu global">
             <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
-            <p class="hero__description">
+            <p class="hero__description hero__couleur">
             <?php bloginfo('description'); ?>
             </p>
-            <p class="hero__courriel">
+            <p class="hero__courriel  hero__couleur">
             <?php bloginfo('admin_email'); ?>
             </p>
-            <p class="hero__adresse">
+            <p class="hero__adresse  hero__couleur">
                 5800 Sherbrooke-est - Montréal (Québec) H1X 2A2
             </p>
-            <p class="hero_auteur">Auteur : <?php echo $hero_auteur ?></p>
+            <p class="hero_auteur  hero__couleur">Auteur : <?php echo $hero_auteur ?></p>
             <div class="hero__icone">
                 <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
                 <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
--- functions.php
+++ functions.php
@@ -28,6 +28,17 @@ $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_b
   'label' => __('Image en arrière-plan', 'theme_tp'),
   'section' => 'hero_section',
 )));
+/////////////////////////// Ajout de la données hero couleur
+$wp_customize->add_setting('hero_couleur', array(
+  'default' => '',
+  'sanitize_callback' => 'esc_url_raw',
+));
+/////////////////////////// Ajout du controle de la donnée
+$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'hero_couleur', array(
+  'label' => __('Couleur du texte', 'theme_tp'),
+  'section' => 'hero_section',
+)));
+
 
 }
 
