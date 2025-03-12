c733094dda6b400ec7c7391614a036f8a9af779b
Valerie Therrien <e6238125@cmaisonneuve.qc.ca>
2025-03-12 10:50:29 -0400

    S8 - Modifications css des single-posts

--- single-post.php
+++ single-post.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-    <h1>-------- single-post.php ----------</h1>
+    <h1><?php the_title(); ?></h1>
     <section class="populaire">
         <div class="global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -8,8 +8,8 @@
                 if (has_post_thumbnail()) {
                 the_post_thumbnail('large'); }
             ?>  
-                <h2><?php the_title(); ?></h2>
                 <div><?php the_content() ?>
+                <?php the_category(); ?>
                 <p>Température maximum: <?php the_field('temperature_maximum') ?>&#176;C</p>
                 <p>Température minimum: <?php the_field('temperature_minimum') ?>&#176;C</p>
                 <p>Température moyenne: <?php the_field('temperature_moyenne') ?>&#176;C</p>
