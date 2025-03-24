1a7bf9ceb189dcd73327c4d479bd047eb11af068
Valerie Therrien <e6238125@cmaisonneuve.qc.ca>
2025-03-24 14:53:17 -0400

    S9 - Récupération du site (après que le site initial soit détruit)

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -15,7 +15,7 @@
     <h4 class="carte__titre"><?php the_title(); ?></h4>
     <p class="carte__description"><?php echo wp_trim_words(get_the_content(),10, " ... " ); ?></p>
     <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">suite ...</a>
-    <p>Température maximum : <?php the_field('temperature_maximum'); ?> C</p>
+    <p>Statut : <?php the_field('statut'); ?> </p>
     <div class="carte__category"><?php  the_category();  ?></div>
   </div>
 </article>
--- readme.md
+++ readme.md
@@ -6,4 +6,4 @@
 
 La réalisation de ce thème permettra d'optimiser la présentation d'un contenu spécifique sur différentes destination de voyages.
 
-#### Site en ligne sur WHC: https://gftnth00.mywhc.ca/4w4_20/
+#### Site en ligne sur WHC: https://gftnth00.mywhc.ca/4w4_54/
