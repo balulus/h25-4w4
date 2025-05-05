85eac2b975c5d24479d404c4c9a2b839f8be7d83
Valerie Therrien <e6238125@cmaisonneuve.qc.ca>
2025-05-05 13:53:28 -0400

    Semaine 14 - Réglage du bug de l'accordéon après la fusion

--- functions/options.php
+++ functions/options.php
@@ -7,7 +7,6 @@ function mon_theme_supports() {
 add_theme_support('title-tag');
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
-add_image_size('logo', 75, 75, true);
 add_theme_support('custom-logo', array(
   'height'      => 250,
   'width'       => 250,
@@ -22,7 +21,7 @@ add_action( 'after_setup_theme', 'mon_theme_supports' );
 function theme_4w4_enqueue_styles() { 
 wp_enqueue_style('normalize', get_template_directory_uri() . '/normalize.css');  
 wp_enqueue_style('mon-style-style', get_stylesheet_uri()); 
-/*
+
 wp_enqueue_script(
 'destination_restapi',
 get_template_directory_uri() . '/js/destination.js',
@@ -31,7 +30,6 @@ filemtime(get_template_directory() .
 '/js/destination.js'),
 true
 );
-*/
 wp_enqueue_script(
     'carrousel',
     get_template_directory_uri() . '/js/carrousel.js',
@@ -64,6 +62,8 @@ function modifie_requete_principal( $query ) {
     }
    }
    add_action( 'pre_get_posts', 'modifie_requete_principal' );
+
+
 
 
 ?>
