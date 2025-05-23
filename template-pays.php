efa8f32f6d3d7e2e32103fe661daad7ad806dfca
Valerie Therrien <e6238125@cmaisonneuve.qc.ca>
2025-05-23 10:01:09 -0400

    Semaine 16 - Examen - Liaison avec la page pays

---
+++ template-pays.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Pays
+ */
+
+
+get_header(); ?>
+
+<div class="intro">
+  <h1><?php the_title(); ?></h1>
+  <p>Plongez au cœur de l’aventure et laissez-vous emporter par l’appel du large ! Notre planète regorge de destinations incroyables, chacune promettant une expérience unique et mémorable. Que vous rêviez de plages idylliques baignées de soleil, de sommets majestueux invitant à la randonnée, de villes vibrantes d’histoire et de modernité, ou de rencontres culturelles authentiques, il y a un pays fait pour vous.</p>
+</div>
+
+<div id="destinations-container"></div>
+
+<?php get_footer(); ?>
